74774837cd36137c055e83c340a389ad8239fe9a
Yang <ywocean@gmail.com>
2016-04-07 23:35:47 -0700

    layout + basic tip calculating logic

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,16 +10,88 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: variables
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var splitByTextField: UITextField!
+    @IBOutlet weak var splitAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalAmountLabel.text = "$0.00"
+        billAmountTextField.text = "$"
+        splitByTextField.text = "1"
+        splitAmountLabel.text = "$0.00"
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: actions
+    
+    @IBAction func onEditingDidBegin(sender: AnyObject) {
+        if let billAmountValueString = billAmountTextField.text {
+            if billAmountValueString == "$" {
+                billAmountTextField.text = "";
+            }
+        }
+        
+        let endPosition = billAmountTextField.endOfDocument
+        billAmountTextField.selectedTextRange = billAmountTextField.textRangeFromPosition(endPosition, toPosition: endPosition)
+    }
+    
+    @IBAction func onEditingChange(sender: AnyObject) {
+        if let billAmountValueString = billAmountTextField.text {
+            if let billAmount = Double(billAmountValueString){
+                let tipAmount = billAmount * 0.2
+                let totalAmount = tipAmount + billAmount;
+                tipLabel.text = "$" + String(tipAmount)
+                totalAmountLabel.text = "$" + String(totalAmount)
+                
+                if let splitByValueString = splitByTextField.text {
+                    if let splitBy = Double(splitByValueString) {
+                        let splitAmount = totalAmount / splitBy
+                        splitAmountLabel.text = "$" + String(splitAmount)
+                        return
+                    }
+                }
+                
+                splitAmountLabel.text = "$-.--"
+                return
+            }
+        }
+        
+        tipLabel.text = "$0.00"
+        totalAmountLabel.text = "$0.00"
+        splitAmountLabel.text = "$0.00"
+    }
+    
+    // MARK: util functions
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+        
+        if let billAmountValueString = billAmountTextField.text {
+            if billAmountValueString == "" {
+                billAmountTextField.text = "$";
+            }
+        }
+        
+        if let splitByValueString = splitByTextField.text {
+            if splitByValueString == "" {
+                splitByTextField.text = "1";
+            }
+        }
+    }
 
 }
-
